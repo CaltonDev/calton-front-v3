b0e4c597181dc87681918fc22d4a70ddfd5a9454
ugodebenedetto <ugo.debenedetto@gmail.com>
2024-09-02 08:27:23 +0200

    fix: bubbles lint

--- src/components/Charts/BubbleCharts/BubbleChartCustom/BubbleChartUI/BubbleChartUI.js
+++ src/components/Charts/BubbleCharts/BubbleChartCustom/BubbleChartUI/BubbleChartUI.js
@@ -1,66 +1,76 @@
-import React from "react";
-import styles from "./BubbleChartUI.module.scss";
-import { useSelector } from "react-redux";
+import React from 'react'
+import styles from './BubbleChartUI.module.scss'
+import { useSelector } from 'react-redux'
 
 function BubbleChartUI(props) {
-  const { data, handleClick } = props;
-  const wordSelected = useSelector((state) => state.SelectedWords);
-  return (
-    <>
-      <div
-        style={{
-          height: `${data.total}%`,
-          width: `${data.total}%`,
-          opacity: data.title === wordSelected.word && wordSelected.sentiment === null ? 0.5 : 1,
-          // height:"100%",
-          //  width:"100%",
-
-        }}
-        className={styles.container}
-      >
-        <div
-          style={{
-            width: `${data.count["positive"]}%`,
-            backgroundColor: "#34e0a1",
-            opacity:
-              data.title === wordSelected.word && wordSelected.sentiment === "1"
-                ? 0.5
-                : 1,
-          }}
-          className={styles.bubbleEl}
-          onClick={() => handleClick("1")}
-        />
-        <div
-          style={{
-            width: `${data.count["neutral"]}%`,
-            backgroundColor: "#fcc207",
-            opacity:
-              data.title === wordSelected.word && wordSelected.sentiment === "0"
-                ? 0.5
-                : 1,
-          }}
-          className={styles.bubbleEl}
-          onClick={() => handleClick("0")}
-        />
-        <div
-          style={{
-            width: `${data.count["negative"]}%`,
-            backgroundColor: "#ff6960",
-            opacity:
-              data.title === wordSelected.word &&
-              wordSelected.sentiment === "-1"
-                ? 0.5
-                : 1,
-          }}
-          className={styles.bubbleEl}
-          onClick={() => handleClick("-1")}
-        />
-        <div className={styles.textView} onClick={() => handleClick(null)}>
-          <label className={styles.labelText}>{data.title}</label>
-          <p className={styles.valueText}>{data.count["positive"]}%</p>
-        </div>
-      </div>
-    </>
-  );
+    const { data, handleClick } = props
+    const wordSelected = useSelector((state) => state.SelectedWords)
+    return (
+        <>
+            <div
+                style={{
+                    height: `${data.total}%`,
+                    width: `${data.total}%`,
+                    opacity:
+                        data.title === wordSelected.word &&
+                        wordSelected.sentiment === null
+                            ? 0.5
+                            : 1,
+                    // height:"100%",
+                    //  width:"100%",
+                }}
+                className={styles.container}
+            >
+                <div
+                    style={{
+                        width: `${data.count['positive']}%`,
+                        backgroundColor: '#34e0a1',
+                        opacity:
+                            data.title === wordSelected.word &&
+                            wordSelected.sentiment === '1'
+                                ? 0.5
+                                : 1,
+                    }}
+                    className={styles.bubbleEl}
+                    onClick={() => handleClick('1')}
+                />
+                <div
+                    style={{
+                        width: `${data.count['neutral']}%`,
+                        backgroundColor: '#fcc207',
+                        opacity:
+                            data.title === wordSelected.word &&
+                            wordSelected.sentiment === '0'
+                                ? 0.5
+                                : 1,
+                    }}
+                    className={styles.bubbleEl}
+                    onClick={() => handleClick('0')}
+                />
+                <div
+                    style={{
+                        width: `${data.count['negative']}%`,
+                        backgroundColor: '#ff6960',
+                        opacity:
+                            data.title === wordSelected.word &&
+                            wordSelected.sentiment === '-1'
+                                ? 0.5
+                                : 1,
+                    }}
+                    className={styles.bubbleEl}
+                    onClick={() => handleClick('-1')}
+                />
+                <div
+                    className={styles.textView}
+                    onClick={() => handleClick(null)}
+                >
+                    <label className={styles.labelText}>{data.title}</label>
+                    <p className={styles.valueText}>
+                        {data.count['positive']}%
+                    </p>
+                </div>
+            </div>
+        </>
+    )
 }
-export default BubbleChartUI;
+export default BubbleChartUI
--- src/components/Charts/BubbleCharts/BubbleChartCustom/nodes.js
+++ src/components/Charts/BubbleCharts/BubbleChartCustom/nodes.js
@@ -1,31 +1,27 @@
-export function generateNodes({
-  bubbles,
-  scale,
-  width,
-}) {
-    const nodes = [];
+export function generateNodes({ bubbles, scale, width }) {
+    const nodes = []
 
     for (const bubble of bubbles) {
-        const { id, total, title: name, count } = bubble;
+        const { id, total, title: name, count } = bubble
         //const MIN_RADIUS = 10;
-        const MIN_RADIUS = 20;
-        const MAX_RADIUS = 80;
+        const MIN_RADIUS = 20
+        const MAX_RADIUS = 80
 
-        let totalR = Math.max(total * scale, MIN_RADIUS + total * scale);
+        let totalR = Math.max(total * scale, MIN_RADIUS + total * scale)
         totalR = Math.min(totalR, MAX_RADIUS)
 
         total > 0 &&
-        nodes.push({
-            bubbleId: `bubble${id}`,
-            initialRadius: totalR,
-            mastery: "positive",
-            name,
-            r: totalR,
-            skillId: id,
-            x: Math.random() * width,
-            y: 0,
-            count: count,
-        });
+            nodes.push({
+                bubbleId: `bubble${id}`,
+                initialRadius: totalR,
+                mastery: 'positive',
+                name,
+                r: totalR,
+                skillId: id,
+                x: Math.random() * width,
+                y: 0,
+                count: count,
+            })
     }
-    return nodes;
+    return nodes
 }
