5dce70009afdb3bf9b96428545ebef94d2fb67c2
Gabriele Ranzieri <104820500+Ranzeb@users.noreply.github.com>
2024-09-03 11:25:40 +0200

    Fix: wrong import

--- src/pages/Luoghi/Luoghi.tsx
+++ src/pages/Luoghi/Luoghi.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../store/store'
 import Table from '../../components/Table/Table'
 import PageHeader from '../../components/PageComponents/PageHeader/PageHeader'
 import PageContainer from '../../components/PageComponents/PageContainer/PageContainer'
 import { useTranslation } from 'react-i18next'
 import FilterService from '../../services/FilterService'
 import { getNoCodeFromPlatfrom } from '../../helpers/helpers'
-import PageNavigator from '../../components/PageNavigator/PageNavigator'
-import styles from './Luoghi.module.scss'
+
 function Luoghi() {
     const { t } = useTranslation()
     const locationData = FilterService.getLocationsFiltered(
