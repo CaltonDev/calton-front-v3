bcd635fcd9bfdad3210b50557fde8ecf5e722d47
ugodebenedetto <ugo.debenedetto@gmail.com>
2024-10-09 10:32:58 +0200

    fix(horizontalANt): missing .config

--- src/components/Charts/HorizontalBar/HorizontalBarAnt.tsx
+++ src/components/Charts/HorizontalBar/HorizontalBarAnt.tsx
@@ -4,7 +4,7 @@ import { Column } from '@ant-design/plots'
 function HorizontalBar(config: any) {
     return (
         <div style={{ width: '100%' }}>
-            <Column {...config} />
+            <Column {...config.config} />
         </div>
     )
 }
